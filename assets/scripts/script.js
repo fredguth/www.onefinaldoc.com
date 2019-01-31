4dc105070d1d4eba00c5805b71121b16199bbe9f
Caê Penna <caepenna@gmail.com>
2019-01-31 18:04:46 -0200

    Header animation

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,9 +1,15 @@
 $(document).on('scroll', function() {
-  if($(document).scrollTop()>96) {
+
+  if($(document).scrollTop()>64&&$(document).scrollTop()<128) {
+    $('.ofd-header').addClass('-hide');
+  } else {
+    $('.ofd-header').removeClass('-hide');
+  }
+
+  if ($(document).scrollTop()>128) {
     $('.ofd-header').addClass('-fix');
   } else {
     $('.ofd-header').removeClass('-fix');
   }
-});
 
-console.log('oi')
+});
