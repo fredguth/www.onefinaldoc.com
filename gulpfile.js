b6dce89866f92a8d37b45384518c8716f855d82f
Caê Penna <caepenna@gmail.com>
2019-01-31 15:25:25 -0200

    Remove comments from gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,9 @@
-#
-# Gulpfile with:
-#
-# - Slim
-# - Sass
-# - Browsersync
-# - CSS and HTML compression
-#
-# Install dependencies:
-#
-#   $ npm install gulp gulp-concat gulp-uglify event-stream gulp-sass gulp-cssmin browser-sync gulp-util gulp-shell
-#
-# Then start developing:
-#
-#   $ gulp
-#
-
 gulp        = require 'gulp'
 concat      = require 'gulp-concat'
 es          = require('event-stream')
 sass        = require 'gulp-sass'
 uglify      = require 'gulp-uglify'
-streamqueue = require 'streamqueue' # Preserves file order (vendor...)
+streamqueue = require 'streamqueue'
 gutil       = require 'gulp-util'
 shell       = require 'gulp-shell'
 cssmin      = require 'gulp-cssmin'
@@ -39,34 +22,30 @@ targets =
   html: 'www/'
   js: 'www/js'
 
-# Compile Slim
 gulp.task 'slim', ->
   gulp.src(sources.html)
-    .pipe(shell(["slimrb -r ./lib/helpers.rb -p <%= file.path %> > ./#{targets.html}/<%= file.relative.replace(\".slim\", \".html\") %>"]))
+    .pipe(shell(["slimrb -r ./lib/helpers.rb -p <%= file.path %> > .
 
-# Compile CSS
 gulp.task 'css', ->
   stream = streamqueue(objectMode: true)
-  # Vendor files
+
   stream.queue(gulp.src(sources.css))
-  # App files
+
   stream.queue(gulp.src(sources.sass).pipe(sass(style: 'expanded', includePaths: ['src/css'], errLogToConsole: true)))
   stream.done()
     .pipe(concat("all.css"))
     .pipe(if isProd then uglify() else gutil.noop())
     .pipe(gulp.dest(targets.css))
 
-# Reload browser
 gulp.task 'server', ->
   browserSync.init null,
     open: true
     server:
       baseDir: targets.html
-    reloadDelay: 2000 # Prevent white screen of death
+    reloadDelay: 2000
     watchOptions:
       debounceDelay: 1000
 
-# Watch files for changes
 gulp.task 'watch', ->
   gulp.watch sources.js, ['js']
   gulp.watch sources.css, ['css']
@@ -74,8 +53,6 @@ gulp.task 'watch', ->
   gulp.watch 'www/**/**', (file) ->
     browserSync.reload(file.path) if file.type is "changed"
 
-# Build everything
 gulp.task 'build', ['lint', 'js', 'css', 'slim']
 
-# Start a server and watch for file changes
 gulp.task 'default', ['watch', 'server']
