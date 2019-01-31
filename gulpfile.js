4dafedd10dd4993b686926affcb019e431cb8669
Caê Penna <caepenna@gmail.com>
2019-01-31 16:11:21 -0200

    Final gulp setup with slim

--- gulpfile.js
+++ gulpfile.js
@@ -12,6 +12,7 @@ const plumber = require("gulp-plumber");
 const postcss = require("gulp-postcss");
 const rename = require("gulp-rename");
 const sass = require("gulp-sass");
+const slim = require("gulp-slim");
 
 // BrowserSync
 function browserSync(done) {
@@ -38,8 +39,8 @@ function clean() {
 // Optimize Images
 function images() {
   return gulp
-    .src("./assets/img/**/*")
-    .pipe(newer("./_site/assets/img"))
+    .src("./assets/images/**/*")
+    .pipe(newer("./_site/assets/images"))
     .pipe(
       imagemin([
         imagemin.gifsicle({ interlaced: true }),
@@ -55,44 +56,58 @@ function images() {
         })
       ])
     )
-    .pipe(gulp.dest("./_site/assets/img"));
+    .pipe(gulp.dest("./_site/assets/images"));
 }
 
 // CSS task
 function css() {
   return gulp
-    .src("./assets/scss/**/*.scss")
+    .src("./assets/styles/**/*.sass")
     .pipe(plumber())
     .pipe(sass({ outputStyle: "expanded" }))
-    .pipe(gulp.dest("./_site/assets/css/"))
+    .pipe(gulp.dest("./_site/assets/"))
     .pipe(rename({ suffix: ".min" }))
     .pipe(postcss([autoprefixer(), cssnano()]))
-    .pipe(gulp.dest("./_site/assets/css/"))
+    .pipe(gulp.dest("./_site/assets/"))
+    .pipe(browsersync.stream());
+}
+
+// Markup task
+function html() {
+  return gulp
+    .src("./assets/views/**/*.slim")
+    .pipe(plumber())
+    .pipe(slim({ pretty: true }))
+    .pipe(gulp.dest("./_site/"))
+    .pipe(browsersync.stream());
+}
+
+// Transpile, concatenate and minify scripts
+function scripts() {
+  return gulp
+    .src(["./assets/scripts/**/*.js"])
+    .pipe(plumber())
+    .pipe(gulp.dest("./_site/assets/"))
     .pipe(browsersync.stream());
 }
 
 // Watch files
 function watchFiles() {
-  gulp.watch("./assets/scss/**/*", css);
-  gulp.watch(
-    [
-      "./_includes/**/*",
-      "./_layouts/**/*",
-      "./_pages/**/*",
-      "./_posts/**/*",
-      "./_projects/**/*"
-    ]
-  );
-  gulp.watch("./assets/img/**/*", images);
+  gulp.watch("./assets/views/**/*", html);
+  gulp.watch("./assets/styles/**/*", css);
+  gulp.watch("./assets/images/**/*", images);
+  gulp.watch("./assets/scripts/**/*", scripts);
 }
 
 // define complex tasks
-const build = gulp.series(clean, gulp.parallel(css, images));
+const build = gulp.series(clean, gulp.parallel(html, css, images, scripts));
 const watch = gulp.parallel(watchFiles, browserSync);
 
 // export tasks
-exports.images = images;
+exports.html = html;
 exports.css = css;
+exports.images = images;
+exports.scripts = scripts;
 exports.clean = clean;
 exports.build = build;
 exports.watch = watch;
